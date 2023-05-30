c26f2db07376b355aa83857a9f87da4d88ad5409
Stepan Osadsi <osstep3@gmail.com>
2023-05-30 16:01:23 +0300

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,73 @@
-import { useMemo, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import './App.css'
-import PostItem from './components/PostItem'
 import PostList from './components/PostList'
 import MyButton from './components/UI/button/MyButton'
-import MyInput from './components/UI/input/MyInput'
 import PostForm from './components/PostForm'
-import MySelect from './components/UI/select/MySelect'
 import PostFilter from './components/PostFilter'
+import MyModal from './components/UI/MyModal/MyModal'
+import { usePosts } from './hooks/usePosts'
+import PostService from './API/PostService'
+import Loader from './components/UI/Loader/Loader'
+import { useFetching } from './hooks/useFetching'
 
 function App() {
-  const [posts, setPosts] = useState([
-    { id: 1, title: 'aa', body: 'ee' },
-    { id: 2, title: 'bb 2', body: 'ff' },
-    { id: 3, title: 'cc 3', body: 'aa' },
-  ])
+  const [posts, setPosts] = useState([])
   const [filter, setFilter] = useState({
     sort: '',
     query: '',
   })
+  const [modal, setModal] = useState(false)
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
+  const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+    const posts = await PostService.getAll()
+    console.log(posts);
+    setPosts(posts)
+  })
 
-  const sortedPosts = useMemo(() => {
-    console.log('вызов')
-    if (filter.sort) {
-      return [...posts].sort((a, b) =>
-        a[filter.sort].localeCompare(b[filter.sort])
-      )
-    }
-    return posts
-  }, [filter.sort, posts])
-
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter((post) =>
-      post.title.toLowerCase().includes(filter.query.toLowerCase())
-    )
-  }, [filter.query, sortedPosts])
+  useEffect(() => {
+    fetchPosts()
+  }, [])
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
+    setModal(false)
   }
 
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id))
   }
 
+
   return (
     <div className="App">
-      <PostForm create={createPost} />
+      <MyButton
+        style={{ marginTop: '30px' }}
+        onClick={() => setModal(true)}
+      >
+        Создать пост
+      </MyButton>
+      <MyModal
+        visible={modal}
+        setVisible={setModal}
+      >
+        <PostForm create={createPost} />
+      </MyModal>
+
       <hr style={{ margin: '15px 0' }}></hr>
       <PostFilter
         filter={filter}
         setFilter={setFilter}
       />
-
-      <PostList
-        remove={removePost}
-        posts={sortedAndSearchedPosts}
-        title={'Список постов 1'}
-      />
+      {postError && <h1>Произошла ошибка {postError}</h1>}
+      {isPostsLoading ? (
+      <div style={{display: 'flex', justifyContent: 'center', marginTop: '25px'}}><Loader /></div>
+      ) : (
+        <PostList
+          remove={removePost}
+          posts={sortedAndSearchedPosts}
+          title={'Список постов'}
+        />
+      )}
     </div>
   )
 }
---
+++ src/components/UI/MyModal/MyModal.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import classes from './MyModal.module.css'
+
+const MyModal = ({ children, visible, setVisible }) => {
+  const rootClasses = [classes.myModal]
+  if (visible) {
+    rootClasses.push(classes.active)
+    console.log(rootClasses)
+  }
+
+  return (
+    <div
+      className={rootClasses.join(' ')}
+      onClick={() => setVisible(false)}
+    >
+      <div
+        className={classes.myModalContent}
+        onClick={(e) => e.stopPropagation()}
+      >
+        {children}
+      </div>
+    </div>
+  )
+}
+
+export default MyModal
